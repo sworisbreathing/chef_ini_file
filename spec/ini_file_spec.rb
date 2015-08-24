e393cb9ad8f19ba7df3a64aa3eb0819178b9c48d
Steven Swor <sworisbreathing@gmail.com>
2015-08-24 20:39:19 +1000

    test default attributes

--- spec/ini_file_spec.rb
+++ spec/ini_file_spec.rb
@@ -9,8 +9,10 @@ describe 'lwrp_test::default' do
     allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_return(true)
   end
 
-  it 'creates an ini file' do
-    expect(chef_run).to create_file('/tmp.ini')
+  it 'creates an ini file with default attributes' do
+    expect(chef_run).to create_file('/tmp.ini').with(
+      atomic_update: true
+    )
   end
 
   it 'creates an ini file with explicit attributes' do
